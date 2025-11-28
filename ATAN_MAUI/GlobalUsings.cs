bd8d07adae0c746dc5043bc99aa15fb75c5b2920
Atankeu Tchakoute Ange <natanahel@panus.cm>
2025-11-28 18:08:47 +0100

    Created Landing page for ATAN_MAUI project.

--- ATAN_MAUI/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using ATAN_MAUI.Data;
-global using ATAN_MAUI.PageModels;
-global using ATAN_MAUI.Pages;
-global using ATAN_MAUI.Services;
-global using ATAN_MAUI.Utilities;
-global using Fonts;
